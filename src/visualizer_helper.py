812dab0b3696d3cda501db879bee761f479ec438
Andrew Shea <ashea2nd@mit.edu>
2020-01-13 17:16:26 -0500

    3d plot

--- src/visualizer_helper.py
+++ src/visualizer_helper.py
@@ -8,13 +8,14 @@ import torch
 from torch import nn, optim
 
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 class Visualizer:
-    def tsne_embedding(self, X, n_components=2):
+    def tsne_embedding(self, X, n_components: int=2):
         return TSNE(n_components=n_components, method="barnes_hut").fit_transform(X)
 
-    def umap_embedding(self, X):
-        return UMAP().fit_transform(X)
+    def umap_embedding(self, X, n_components: int=2):
+        return UMAP(n_components=n_components).fit_transform(X)
 
     def plot2d(self, X, filename, colors=None):
         plt.figure(figsize=(8,5))
@@ -25,8 +26,28 @@ class Visualizer:
             cmap='cool', 
             alpha=0.05
         )
-        plt.xlabel('tsne-one')
-        plt.ylabel('tsne-two')
+        plt.xlabel('component-one')
+        plt.ylabel('component-two')
+        plt.colorbar()
+        
+        plt.savefig("{}.png".format(filename))
+        plt.show()
+
+    def plot3d(self, X, filename, colors=None):
+        fig = plt.figure(figsize=(8,5))
+        ax = Axes3D(fig)
+
+        ax.scatter(
+            x=X[:, 0], 
+            y=X[:, 1],
+            z=X[:, 2],
+            c=colors, 
+            cmap='cool', 
+            alpha=0.05
+        )
+        ax.set_xlabel('component-one')
+        ax.set_ylabel('component-two')
+        ax.set_zlabel('component-three')
         plt.colorbar()
         
         plt.savefig("{}.png".format(filename))
