2695986fa056efcf5fea7d04354479f03f665a4f
Andrew Shea <ashea2nd@mit.edu>
2020-03-15 22:09:15 -0400

    asdf

--- src/cluster_helper.py
+++ src/cluster_helper.py
@@ -2,8 +2,10 @@ from collections import Counter
 from wordcloud import WordCloud
 import numpy as np
 import pandas as pd
+from tqdm import tqdm
 import matplotlib.pyplot as plt
 from nltk.corpus import stopwords
+from scipy.sparse import csr_matrix
 import nltk
 nltk.download('stopwords')
 
@@ -59,6 +61,76 @@ class ClusterProcessor():
 
         return all_word_freqs
 
+    def get_cluster_labels(self, mc_cluster=False):
+        if mc_cluster:
+            cluster_labels = self.cluster_assignments['MC_CLUSTER'].values
+        else:
+            cluster_labels = self.cluster_assignments['CLUSTER'].values
+        return cluster_labels 
+
+    def build_cluster_matrix(self, mc_cluster=False):
+        cluster_labels = self.get_cluster_labels(mc_cluster)
+        knn_clusters = np.unique(cluster_labels).shape[0]
+        K = np.zeros((cluster_labels.shape[0], knn_clusters))
+        for i, c in enumerate(cluster_labels):
+            K[i, c] = 1
+        return csr_matrix(K)
+
+    def sort_by_cluster_membership(self, M, mc_cluster=False):
+        cluster_labels = self.get_cluster_labels(mc_cluster)
+        sorted_cluster_labels = sorted(list(enumerate(cluster_labels.tolist())), key=lambda p: p[1])
+        clusters_by_idx = [p[0] for p in sorted_cluster_labels]
+        return M[clusters_by_idx][:, clusters_by_idx]
+
+    def compute_centroids(self, y, mc_cluster=False):
+
+        cluster_labels = self.get_cluster_labels(mc_cluster)
+        cluster_memberships = {}
+        for idx, c in enumerate(cluster_labels):
+            if c not in cluster_memberships:
+                cluster_memberships[c] = [idx]
+            else:
+                cluster_memberships[c].append(idx)
+        
+        unique_labels = np.unique(cluster_labels)
+        centroids = np.zeros((unique_labels.shape[0], y.shape[-1]))
+        for c in tqdm(unique_labels):
+            cluster_c_idx = cluster_memberships[c]
+            cluster_c_embeddings = y[cluster_c_idx]
+            # centroids.append(np.mean(cluster_c_embeddings, axis=0))
+            centroids[c] = np.mean(cluster_c_embeddings, axis=0)
+            if cluster_c_embeddings.shape[0] == 0:
+                print("Empty cluster found:", c)
+        return np.array(centroids)
+
+    def top_diseases_in_cluster(self, cluster, topk=3):
+        remaining_patient_idxs = self.cluster_assignments[self.cluster_assignments["CLUSTER"] == cluster]["ORIGINAL_INDEX"].values
+        remaining_patient_icd_binary = self.patient_icd_binary[remaining_patient_idxs]
+        disease_distribution = np.sum(remaining_patient_icd_binary, axis=0).tolist()[0]
+
+        icdidx_count_topk = sorted(list(enumerate(disease_distribution)), key=lambda p: p[1], reverse=True)[:topk]
+        
+        icdidx = [p[0] for p in icdidx_count_topk]
+        disease_counts_topk = [p[1] for p in icdidx_count_topk]
+        
+        icd9codes_topk = [self.icd9codes.iloc[idx]["ICD9_CODE"] for idx in icdidx]
+        # print(icd9codes_topk)
+        # print(self.icd9diag[self.icd9diag['ICD9_CODE'] == '9974']["LONG_TITLE"].values)
+
+        titles = []
+        for icd9code in icd9codes_topk:
+            title = self.icd9diag[self.icd9diag['ICD9_CODE'] == icd9code]["LONG_TITLE"].values
+            if len(title) == 0:
+                titles.append(icd9code)
+            else:
+                titles.append(title[0])
+        # titles = list(map(lambda icd9code: self.icd9diag[self.icd9diag['ICD9_CODE'] == icd9code]["LONG_TITLE"].values[0], icd9codes_topk))
+
+        return pd.DataFrame({"ICD9_CODE": icd9codes_topk,
+                             "LONG_TITLE": titles,
+                             "DISEASE_COUNT": disease_counts_topk})
+        # return list(zip(icd9codes_topk, titles, disease_counts_topk))
+
     def plot_disease_distribution(self, topk, cluster=None, plot=True):
         if cluster == None:
             remaining_patient_idxs = self.cluster_assignments["ORIGINAL_INDEX"].values
@@ -75,7 +147,16 @@ class ClusterProcessor():
         disease_counts_topk = [p[1] for p in icdidx_count_topk]
         
         icd9codes_topk = [self.icd9codes.iloc[idx]["ICD9_CODE"] for idx in icdidx]
-        titles = list(map(lambda icd9code: self.icd9diag[self.icd9diag['ICD9_CODE'] == icd9code]["LONG_TITLE"].values[0], icd9codes_topk))
+
+        titles = []
+
+        for icd9code in icd9codes_topk:
+            title = self.icd9diag[self.icd9diag['ICD9_CODE'] == icd9code]["LONG_TITLE"].values
+            if len(title) == 0:
+                titles.append(icd9code)
+            else:
+                titles.append(title[0])
+        # titles = list(map(lambda icd9code: self.icd9diag[self.icd9diag['ICD9_CODE'] == icd9code]["LONG_TITLE"].values[0], icd9codes_topk))
 
         if plot:
             plt.figure(figsize=(5, 10))
