ad134c12f8e76a3ad91e908be596238d6a0d7014
Andrew Shea <ashea2nd@mit.edu>
2020-04-04 16:38:28 -0400

    Heatmaps

--- experiments/200402_spectral_analysis_K50_rescaled_svd_knn50 copy/sorted_adjacency_heatmap.py
+++ experiments/200402_spectral_analysis_K50_rescaled_svd_knn50 copy/sorted_adjacency_heatmap.py
@@ -15,12 +15,15 @@ def plot_heatmap(sorted_m, matname):
 	sorted_m.data = np.where(sorted_m.data > 0, 1, 0)
 
 	plt.figure(figsize=(80,60))
-	sns.heatmap(sorted_m.toarray(), cmap="Greys", cbar=False)
+
+	idx = np.array([i for i in range(A_sorted.shape[0]) if i % 5 == 0])
+
+	sns.heatmap(sorted_m[idx][:, idx].toarray(), cmap="Greys", cbar=False)
 	fontsize = 110
 	plt.xlabel("Patients sorted by cluster membership", fontsize=fontsize)
 	plt.ylabel("Patients sorted by cluster membership", fontsize=fontsize)
 	plt.tight_layout()
 	plt.savefig("./tests/heatmap_{}.png".format(matname))
 
-plot_heatmap(A_sorted, "A_sorted")
+# plot_heatmap(A_sorted, "A_sorted")
 plot_heatmap(A_mc_sorted, "A_mc_sorted")
