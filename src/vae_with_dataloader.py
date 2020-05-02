b5f7fb6e5078e553b09f4d7c27f0b2540cd21a78
Andrew Shea <ashea2nd@mit.edu>
2020-05-02 17:15:48 -0400

    vae experiement

--- src/vae_with_dataloader.py
+++ src/vae_with_dataloader.py
@@ -166,7 +166,7 @@ class VAETrainerWithDataLoader:
         # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
         # https://arxiv.org/abs/1312.6114
         # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
-        KLD = -self.kld_beta*0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        KLD = 0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
 
         return BCE + KLD, BCE, KLD
 
